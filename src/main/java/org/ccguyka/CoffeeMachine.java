014a15aad4863cb964f0ea67fab27c1c8bfd8d23
ccguyka <ccguyka@gmail.com>
2016-01-10 15:05:45 +0100

    format code to repace tabs with spaces

--- src/main/java/org/ccguyka/CoffeeMachine.java
+++ src/main/java/org/ccguyka/CoffeeMachine.java
@@ -4,35 +4,35 @@ import org.ccguyka.nested.MissingIngrediantException;
 
 public class CoffeeMachine {
 
-	private String beans;
-	private boolean hasWater;
-	private Coffee coffee;
+    private String beans;
+    private boolean hasWater;
+    private Coffee coffee;
 
-	public void addBeans(String beans) {
-		this.beans = beans;
-	}
+    public void addBeans(String beans) {
+        this.beans = beans;
+    }
 
-	public void addWater() {
-		hasWater = true;
-	}
+    public void addWater() {
+        hasWater = true;
+    }
 
-	public void brew() throws MissingIngrediantException {
-		checkIngrediants();
+    public void brew() throws MissingIngrediantException {
+        checkIngrediants();
 
-		coffee = new Coffee();
-	}
+        coffee = new Coffee();
+    }
 
-	private void checkIngrediants() throws MissingIngrediantException {
-		if (!hasWater) {
-			throw new MissingIngrediantException("Water");
-		}
+    private void checkIngrediants() throws MissingIngrediantException {
+        if (!hasWater) {
+            throw new MissingIngrediantException("Water");
+        }
 
-		if (beans == null) {
-			throw new MissingIngrediantException("Beans");
-		}
-	}
+        if (beans == null) {
+            throw new MissingIngrediantException("Beans");
+        }
+    }
 
-	public Coffee getCoffee() {
-		return coffee;
-	}
+    public Coffee getCoffee() {
+        return coffee;
+    }
 }
--- src/main/java/org/ccguyka/nested/MissingIngrediantException.java
+++ src/main/java/org/ccguyka/nested/MissingIngrediantException.java
@@ -2,15 +2,15 @@ package org.ccguyka.nested;
 
 public class MissingIngrediantException extends Exception {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private final String ingrediant;
+    private final String ingrediant;
 
-	public MissingIngrediantException(String ingrediant) {
-		this.ingrediant = ingrediant;
-	}
+    public MissingIngrediantException(String ingrediant) {
+        this.ingrediant = ingrediant;
+    }
 
-	public String getMissingIngrediant() {
-		return ingrediant;
-	}
+    public String getMissingIngrediant() {
+        return ingrediant;
+    }
 }
